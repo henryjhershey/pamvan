be5f2ee904a38d52362c1b2737ad6708d2434cfc
Henry Hershey <34039859+henryjhershey@users.noreply.github.com>
2021-12-04 13:25:49 -0600

    add stochasticity

--- source/recruit.R
+++ source/recruit.R
@@ -1,7 +1,8 @@
 
 #make a function for adding new recruits to each reservoir each year
 recruit = function(abund,starts){
-  newfish = round(starts$R*abund)
+  R = max(c(0,rnorm(starts$R,starts$Rvar)))
+  newfish = round(R*abund)
   nnew = sum(newfish[c("1","2","3","4")],na.rm=T)
   resnew = as.numeric(names(newfish)[names(newfish)>0])
   recruits = matrix(c(rep(resnew,newfish[c(as.character(resnew))]),rep(0,nnew)),nrow=nnew,ncol=2,byrow=F)
