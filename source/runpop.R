81119e520e4c0a460630fe7d4b841227998c8125
Henry Hershey <34039859+henryjhershey@users.noreply.github.com>
2022-04-25 14:46:22 -0500

    add downstream movement for spawners

--- source/runpop.R
+++ source/runpop.R
@@ -12,10 +12,10 @@ abund = list(NA)
                              max(pop[[t-1]][i,1]-rbinom(1,1,starts$pdown),1)*rbinom(1,1,1-starts$MJ), #age 0s either move down (or not) and die (or not)
                              ifelse(pop[[t-1]][i,2]>0 & pop[[t-1]][i,2]<8,
                                     pop[[t-1]][i,1]*rbinom(1,1,1-starts$MA), #if the fish is older than 0 but younger than 8, die or survive, no move
-                                    ifelse(pop[[t-1]][i,1]==0,0,min(pop[[t-1]][i,1]+rbinom(1,1,starts$pup),4)*rbinom(1,1,1-starts$MA)))) #if the fish is older than 7, move up (or not) and die (or not) 
+                                    ifelse(pop[[t-1]][i,1]==0,0,min(pop[[t-1]][i,1]+rbinom(1,1,starts$pup[pop[[t-1]][i,1]]),4)*rbinom(1,1,1-starts$MA)))) #if the fish is older than 7, move up (or not) and die (or not) 
       
     }
-    abund[[t]] <- table(factor(pop[[t]][,1],levels=0:starts$nres))#after each year, how many fish are in each reservoir (or dead)
+    abund[[t]] <- table(factor(pop[[t]][,1],levels=0:starts$nres)) #after each year, how many fish are in each reservoir (or dead)
     pop[[t]] = rbind(recruit(abund[[t]],starts),pop[[t]][pop[[t]][,1]>0,]) #add new recruits to the population
   } else {
     abund[[t]] = c(0,0,0,0,0)
@@ -25,5 +25,15 @@ abund = list(NA)
   abund = melt(abund)
   colnames(abund)=c("N","Res","T")
   return(abund)
+  #allow the migrators to move back down to the reservoir they came from after they get counted for the annual abundance estimate. this way, they contribute to the recruitment of the 
+  #reservoir they migrated to, but they can move back downstream after.
+  for(t in 2:starts$nt){
+    for(i in 1:dim(pop)[1]){
+    pop[[t]][i,1] = ifelse(pop[[t-1]][i,1]<pop[[t]][i,1] & pop[[t]][i,1] > 0,pop[[t]][i,1]-rbinom(1,1,starts$pdown),pop[[t]][i,1])
   }
+  }
+  
+  
+  }
+
 
