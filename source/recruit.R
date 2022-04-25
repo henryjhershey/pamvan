2ac04a962b663fc0ed66d3aeadd14e9f58168b61
Henry Hershey <34039859+henryjhershey@users.noreply.github.com>
2022-04-25 14:46:55 -0500

    add carrying capacity

--- source/recruit.R
+++ source/recruit.R
@@ -2,7 +2,9 @@
 #make a function for adding new recruits to each reservoir each year
 recruit = function(abund,starts){
   R = max(c(0,rnorm(starts$R,starts$Rvar)))
-  newfish = round(R*abund)
+  #newfish = ifelse(starts$dens==T, starts$K/(1+((starts$K-abund)/abund)*exp(-R)), round(abund*R))
+  newfish= round(R*abund)
+  #newfish= round(starts$K/(1+((starts$K-abund)/abund)*exp(-R)))
   nnew = sum(newfish[c("1","2","3","4")],na.rm=T)
   resnew = as.numeric(names(newfish)[names(newfish)>0])
   recruits = matrix(c(rep(resnew,newfish[c(as.character(resnew))]),rep(0,nnew)),nrow=nnew,ncol=2,byrow=F)
